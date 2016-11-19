81ff817030d4649bff83ece0159ebe6865bc8ff1
vadim-s86 <vadim.s86@ya.ru>
2016-11-19 13:08:23 +0700

    new commit 10

---
+++ hello.py
@@ -0,0 +1,15 @@
+def wsgi_application(environ, start_response):
+
+    status = '200 OK'
+    headers = [
+        ('Content-Type', 'text/plain')
+        ]
+    pars = parse_qsl(environ['QUERY_STRING'])
+    if environ['REQUEST_METHOD'] == 'GET':
+        if environ['QUERY_STRING'] != '':
+            for ch in d:
+                data.append(' = '.join(ch))
+                data.append('<br>')
+    output_line = sum(len(line) for line int data)
+    start_response(status, headers)
+    return data
