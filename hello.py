2d6a7f714fec9dedd6e300bee305fe89ebe6c3bc
vadim-s86 <vadim.s86@ya.ru>
2016-11-19 14:15:59 +0700

    new commit 12

--- hello.py
+++ hello.py
@@ -1,15 +1,7 @@
-from cgi import parse_qsl, escape
-def wsgi_application(environ, start_response):
-
-    status = '200 OK'
-    headers = [
-        ('Content-Type', 'text/plain')
-        ]
-    pars = parse_qsl(environ['QUERY_STRING'])
-    if environ['REQUEST_METHOD'] == 'GET':
-        if environ['QUERY_STRING'] != '':
-            for ch in d:
-                data.append(' = '.join(ch))
-                data.append('<br>')
-    start_response(status, headers)
-    return data
+#!/usr/bin/python
+def app(environ, start_response):
+    data = ''
+    for line in environ["QUERY_STRING"].split("&"):
+        data = data+line+"\n"
+    start_response('200 OK', [('Content-Type', 'text/plain')])
+    return [data]
