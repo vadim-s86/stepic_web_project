c5cde727b719ba87433204ab0d67ff1f480ac842
vadim-s86 <vadim.s86@ya.ru>
2016-11-19 14:33:45 +0700

    new commit 15

--- etc/hello.py
+++ etc/hello.py
@@ -1,12 +1,6 @@
 CONFIG = {
-    'mode': 'wsgi',
-    'working_dir': '/home/box/web',
-    # 'python': '/usr/bin/python',
-    'args': (
-        '--bind=0.0.0.0:8080',
-        '--daemon',
-        '--workers=2',
-        '--timeout=60',
-        'hello:app',
-    ),
-}
+  'working_dir': '/home/box/web',
+  'args': (
+   'bind = "0.0.0.0:8080"'
+   ),
+ }
