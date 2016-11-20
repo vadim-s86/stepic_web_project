b74fd0949b09e85aff59b109ed0082f081750141
vadim-s86 <vadim.s86@ya.ru>
2016-11-20 05:21:10 +0000

    django project

--- init.sh
+++ init.sh
@@ -2,6 +2,6 @@ sudo unlink /etc/nginx/sites-enabled/default
 sudo ln -sf /home/box/web/etc/nginx.conf  /etc/nginx/sites-enabled/test.conf
 sudo /etc/init.d/nginx restart
 sudo ln -sf /home/box/web/etc/hello.py /etc/gunicorn.d/hello.py
-sudo gunicorn -c /etc/gunicorn.d/hello.py hello:application
+sudo ln -sf /home/box/web/etc/gunicorn2.conf /etc/gunicorn.d/gunicorn2.conf
 sudo /etc/init.d/gunicorn restart
 #sudo /etc/init.d/mysql start
