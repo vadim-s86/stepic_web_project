780a1dd3b0c8107bf5cccf12ba4fdbeb81e1c1dd
vadim-s86 <vadim.s86@ya.ru>
2016-11-20 06:38:49 +0000

    django project

---
+++ etc/ask.py
@@ -0,0 +1,18 @@
+CONFIG = {
+    'mode': 'django',
+    'environment': {
+        'PYTHONPATH': '/home/box/web/ask',
+    },
+    # 'working_dir': '/',
+
+    # 'user': 'www-data',
+
+    # 'group': 'www-data',
+    'args': (
+        '--bind=0.0.0.0:8000',
+        '--workers=4',
+        # '--worker-class=egg:gunicorn#sync',
+        # '--timeout=30',
+        'ask.settings',
+    ),
+}
--- init.sh
+++ init.sh
@@ -2,7 +2,7 @@ sudo unlink /etc/nginx/sites-enabled/default
 sudo ln -sf /home/box/web/etc/nginx.conf  /etc/nginx/sites-enabled/test.conf
 sudo /etc/init.d/nginx restart
 #sudo ln -sf /home/box/web/etc/hello.py /etc/gunicorn.d/hello.py
-sudo ln -sf /home/box/web/etc/gunicorn_ask.conf /etc/gunicorn.d/gunicorn_ask
+sudo ln -sf /home/box/web/etc/ask.py /etc/gunicorn.d/gunicorn_ask.py
 sudo /etc/init.d/gunicorn restart
 sudo /etc/init.d/mysql start
 git config --global user.email "vadim.s86@ya.ru"
