db47b3b86754819cad3609e98bdd0c3c6bae782d
vadim-s86 <vadim.s86@ya.ru>
2016-11-22 11:50:43 +0700

    django project 2.3 step 9-4

--- ask/qa/models.py
+++ ask/qa/models.py
@@ -2,21 +2,24 @@ from django.db import models
 from django.contrib.auth.models import User
 
 # Create your models here.
+class QuestionManager(models.Manager):
+    def new():
+        pass
+
+    def popular():
+        pass
+
 class Question(models.Model):
-	QuestionManager = models.Manager()
-	title = models.CharField(max_length=255)
-	text = models.TextField()
-	added_at = models.DateTimeField(blank=True, auto_now_add=True)
-	rating = models.IntegerField(default=0)
-	author = models.ForeignKey(User, default='x')
-	likes = models.ManyToManyField(related_name='question_like_user')
-	def new():
-		pass
-	def popular():
-		pass
+    object = QuestionManager();
+    title = models.CharField(max_length=255)
+    text = models.TextField()
+    added_at = models.DateTimeField(blank=True, auto_now_add=True)
+    rating = models.IntegerField(default=0)
+    author = models.ForeignKey(User, default='x')
+    likes = models.ManyToManyField(related_name='question_like_user')
 
 class Answer(models.Model):
-	text = models.TextField()
-	added_at = models.DateTimeField(auto_now_add=True)
-	question = models.ForeignKey(Question, null=True, on_delete=models.SET_NULL)
-	author = models.ForeignKey(User, default='x')
+    text = models.TextField()
+    added_at = models.DateTimeField(auto_now_add=True)
+    question = models.ForeignKey(Question, null=True, on_delete=models.SET_NULL)
+    author = models.ForeignKey(User, default='x')
