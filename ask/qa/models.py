e84eb3b53b5084d570341e63d8f093c9e9b2baa0
vadim-s86 <vadim.s86@ya.ru>
2016-11-22 11:16:25 +0700

    django project 2.3 step 9-4

--- ask/qa/models.py
+++ ask/qa/models.py
@@ -8,16 +8,15 @@ class Question(models.Model):
 	text = models.TextField()
 	added_at = models.DateTimeField(blank=True, auto_now_add=True)
 	rating = models.IntegerField(default=0)
-	author = models.ForeignKey(User.pk)
+	author = models.ForeignKey(User, default='x')
 	likes = models.ManyToManyField(related_name='question_like_user')
 	def new():
 		pass
 	def popular():
 		pass
 
-	
 class Answer(models.Model):
 	text = models.TextField()
 	added_at = models.DateTimeField(auto_now_add=True)
 	question = models.ForeignKey(Question, null=True, on_delete=models.SET_NULL)
-	author = models.ForeignKey(User.pk)
+	author = models.ForeignKey(User, default='x')
