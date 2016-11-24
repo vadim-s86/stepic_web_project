361156e99ebba39deda8b53aaf4cc33e40cf420f
vadim-s86 <vadim.s86@ya.ru>
2016-11-24 16:11:26 +0700

    django project 2.3 step 9-5

--- ask/qa/urls.py
+++ ask/qa/urls.py
@@ -2,16 +2,17 @@ from django.conf.urls import patterns, include, url
 
 from qa.views import test
 
+
 urlpatterns = patterns('',
     # Examples:
     # url(r'^$', 'ask.views.home', name='home'),
     # url(r'^blog/', include('blog.urls')),
 
-    url(r'^$', test, name='test'),
+    url(r'^$', 'qa.views.home', name='home'),
     url(r'login/.*$', test, name='login'),
     url(r'signup/.*$', test, name='signup'),
-    url(r'question/[0-9]+/$', test, name='question'),
+    url(r'question/[0-9]+/$', 'qa.views.question', name='question'),
     url(r'ask/.*$', test, name='ask'),
-    url(r'popular/.*$', test, name='popular'),
+    url(r'popular/.*$', 'qa.views.popular', name='popular'),
     url(r'new/.*$', test, name='new'),
 )
--- ask/qa/views.py
+++ ask/qa/views.py
@@ -1,6 +1,62 @@
-from django.shortcuts import render
+from django.http import HttpResponse, Http404
+from django.core.paginator import Paginator, EmptyPage
+from django.views.decorators.http import require_GET, require_POST
+from django.shortcuts import render, get_object_or_404, redirect
+from qa.models import Question
 
-# Create your views here.
-from django.http import HttpResponse
+
 def test(request, *args, **kwargs):
-	return HttpResponse('OK')
+    return HttpResponse('OK')
+
+
+@require_GET
+def home(request):
+    question = Question.objects.new()
+    paginator, page = Paginator(request, question)
+    render(request, 'qa/home.html', {
+        'question': page.object_list,
+        'paginator': paginator,
+        'page': page,
+    })
+
+
+@require_GET
+def popular(request):
+    question = Question.objects.popular()
+    paginator, page = Paginator(request, question)
+    render(request, 'qa/popular.html', {
+        'question': page.object_list,
+        'paginator': paginator,
+        'page': page,
+    })
+
+
+@require_GET
+def question(request, pk=id):
+    question = get_object_or_404(Question, id=id)
+    return render(request, 'qa/question.html', {
+        'question': question,
+    })
+
+
+def paginate(request, qs):
+    try:
+        limit = int(request.GET.get('limit', 10))
+    except ValueError:
+        limit = 10
+
+    if limit > 100:
+        limit = 10
+
+    try:
+        page = int(request.GET.get('page', 1))
+    except ValueError:
+        raise Http404
+
+    paginator = Paginator(qs, limit)
+    try:
+        page = paginator.page(page)
+    except EmptyPage:
+        page = paginator.page(paginator.num_pages)
+
+    return paginator, page
